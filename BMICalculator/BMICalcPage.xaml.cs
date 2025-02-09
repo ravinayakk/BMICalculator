cd8e51670f9dcae2a6536f8165858ad2ad3a671a
Ravi Nayak <nayakrr@mail.uc.edu>
2025-02-09 16:55:29 -0500

    fixed android emulator and fixed display

--- BMICalculator/BMICalcPage.xaml.cs
+++ BMICalculator/BMICalcPage.xaml.cs
@@ -7,8 +7,29 @@ public partial class BMICalcPage : ContentPage
 		InitializeComponent();
 	}
 
-	private void BtnCalculateBMI_Clicked(object sender, EventArgs e)
+    string choice = "Male";
+    private void TapMale_Tapped(object sender, TappedEventArgs e)
+    {
+        choice = "Male";
+        FrameMale.BorderColor = Color.FromArgb("#0a0e29");
+        FrameFemale.BorderColor = Color.FromArgb("#fdfdfd");
+    }
+    private void TapFemale_Tapped(object sender, TappedEventArgs e)
+    {
+        choice = "Female";
+        FrameFemale.BorderColor = Color.FromArgb("#0a0e29");
+        FrameMale.BorderColor = Color.FromArgb("#fdfdfd");
+    }
+
+    private void BtnCalculateBMI_Clicked(object sender, EventArgs e)
 	{
-		
-	}
+        double height = double.Parse(LblHeight.Text);
+        double weight = double.Parse(LblWeight.Text);
+        double bmi = (weight * 703) / height;
+    }
+
+    private void Btn_Clicked(object sender, EventArgs e)
+    {
+        DisplayAlert("Your calulated BMI results are:","Gender: ",choice , "BMI: ");
+    }
 }
